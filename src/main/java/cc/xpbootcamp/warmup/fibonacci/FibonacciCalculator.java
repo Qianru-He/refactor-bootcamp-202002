c9550d1c480d88b31dacbfe8f9eba388cd32ffd7
Qianru-He <2583543607@qq.com>
2020-02-15 20:33:52 +0800

    calculate more than 50

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/FibonacciCalculator.java
@@ -2,12 +2,20 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class FibonacciCalculator {
 	public static long calculate(int index) {
-		if(index <1 ){
+		if (index < 1 || index > 50) {
 			throw new IllegalArgumentException();
 		}
 		if (index == 1 || index == 2) {
 			return 1;
 		}
-		return calculate(index - 2) + calculate(index - 1);
+		long first = 1;
+		long second = 1;
+		long temp;
+		for (int i = 3; i <= index; i++) {
+			temp = first;
+			first = second;
+			second = temp + second;
+		}
+		return second;
 	}
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -7,26 +7,31 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FibonacciTest {
 	@Test
-	void should_return_1_when_index_1_and_2(){
-		assertEquals(1,FibonacciCalculator.calculate(1));
-		assertEquals(1,FibonacciCalculator.calculate(2));
+	void should_return_1_when_index_1_and_2() {
+		assertEquals(1, FibonacciCalculator.calculate(1));
+		assertEquals(1, FibonacciCalculator.calculate(2));
 	}
 
 	@Test
-	void should_return_corresponding_result(){
-		assertEquals(2,FibonacciCalculator.calculate(3));
-		assertEquals(5,FibonacciCalculator.calculate(5));
-		assertEquals(89,FibonacciCalculator.calculate(11));
+	void should_return_corresponding_result() {
+		assertEquals(2, FibonacciCalculator.calculate(3));
+		assertEquals(5, FibonacciCalculator.calculate(5));
+		assertEquals(89, FibonacciCalculator.calculate(11));
 	}
 
 	@Test
-	void should_return_12586269025_when_index_50(){
-		assertEquals(12586269025L,FibonacciCalculator.calculate(50));
+	void should_return_12586269025_when_index_50() {
+		assertEquals(12586269025L, FibonacciCalculator.calculate(50));
 	}
 
 	@Test
-	void should_throw_exception_when_index_less_than_1(){
-		assertThrows(IllegalArgumentException.class,()->FibonacciCalculator.calculate(0));
-		assertThrows(IllegalArgumentException.class,()->FibonacciCalculator.calculate(-1));
+	void should_throw_exception_when_index_less_than_1() {
+		assertThrows(IllegalArgumentException.class, () -> FibonacciCalculator.calculate(0));
+		assertThrows(IllegalArgumentException.class, () -> FibonacciCalculator.calculate(-1));
+	}
+
+	@Test
+	void should_throw_exception_when_index_more_than_50() {
+		assertThrows(IllegalArgumentException.class, () -> FibonacciCalculator.calculate(58));
 	}
 }
